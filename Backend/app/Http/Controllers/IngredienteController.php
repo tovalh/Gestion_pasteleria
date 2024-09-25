8527565b3d6b1687c5c3cf6dfb1a3116b93c7626
Tovalh <cristobalva16@gmail.com>
2024-09-24 21:14:11 -0300

    Controladores

--- Backend/app/Http/Controllers/IngredienteController.php
+++ Backend/app/Http/Controllers/IngredienteController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class InventarioController extends Controller
+class IngredienteController extends Controller
 {
     //
 }
--- Backend/app/Http/Controllers/ProductoController.php
+++ Backend/app/Http/Controllers/ProductoController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class PedidoController extends Controller
+class ProductoController extends Controller
 {
     //
 }
