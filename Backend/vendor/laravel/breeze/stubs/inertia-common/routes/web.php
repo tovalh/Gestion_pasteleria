46f4b31550a065341ebc4aab27d0c9231e7b050e
Corot <navarrete.benjamin02@gmail.com>
2024-09-27 13:09:46 -0300

    react intento frontend

--- Backend/vendor/laravel/breeze/stubs/inertia-common/routes/web.php
+++ Backend/vendor/laravel/breeze/stubs/inertia-common/routes/web.php
@@ -24,4 +24,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/{any}', function () {
+    return view('app');
+})->where('any', '.*');
+
 require __DIR__.'/auth.php';
