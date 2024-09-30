81112eda7f32d2c9c46a063ee0ee9a21a4b4a00a
Tovalh <cristobalva16@gmail.com>
2024-09-30 19:38:06 -0300

    Migrations creadas

---
+++ pasteleria_breeze_react/database/migrations/2024_09_30_223333_create_producto_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('producto', function (Blueprint $table) {
+            $table->integer('idProducto', true);
+            $table->string('NombreProducto', 35);
+            $table->mediumText('DescripcionProducto');
+            $table->string('PrecioProducto', 12);
+            $table->integer('Seccion_idSeccion')->index('fk_productos_seccion1');
+
+            $table->primary(['idProducto', 'Seccion_idSeccion']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('producto');
+    }
+};
